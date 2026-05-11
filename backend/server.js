11938ed966fe299d7b5da7e7f0e284cf8ad18d48
ElijahGolnik <elijahgolnik@gmail.com>
2026-05-11 14:38:30 -0400

    Added express and began work on a simple server.js running specifically on port 5000. Will continue with Dockerfile and dockerignore next.

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+// node server.js
+const express = require("express");
+
+const app = express()
+const PORT = process.env.PORT || 5000;
+
+// Performing Health Check
+app.get("/health", (req, res) => {
+    res.status(200).send("OK");
+});
+
+// Routing Basic API
+app.get("/api", (req, res) => {
+    res.json({ message: "Backend is running!" });
+});
+
+// Basic Port Listener
+app.listen(PORT, () => {
+    console.log('Server operating on', {PORT} );
+});
